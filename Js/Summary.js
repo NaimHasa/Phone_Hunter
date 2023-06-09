69787a9a765854c19278d8995283e75dad9eca2f
Naim Hasan <naimarafat7@gmail.com>
2023-06-09 06:17:31 -0700

    summary

---
+++ Js/Summary.js
@@ -0,0 +1,37 @@
+/*
+FETCH
+1.Fetch must provide url
+dynamic or static
+2. how to convert fetch promise return to json 
+3. how to convert json to data
+4.[cool headed]
+users:
+-->array of object 
+---> object with property users
+---> Object with a property called data
+
+Nested Object
+
+
+-------------------------
+DOM Manipulation
+1.get something from the DOM
+2.create element to append to the DOM
+3.dynamically load data based on id
+
+array---> forEach, map, find, filter.
+
+
+----------------------------
+Template string
+
+
+
+
+
+
+
+
+
+*/
+
